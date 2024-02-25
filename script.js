19869e2d044029d1a8a6af1febb22081917bf46c
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-02-25 19:35:38 +0100

    add/segundo formulario de prueba creado/objetos en script.js actualizados a una sola respuesta/css modificado a mobile first

---
+++ script.js
@@ -0,0 +1,25 @@
+/* ----- PREGUNTA 1 ----- */
+const respuestas1 = {
+    album1: "kanye"
+};
+
+/* ----- PREGUNTA 2 ----- */
+const respuestas2 = {
+    paris2: "kanye",
+    paris2: "jayz"
+};
+
+/* ----- PREGUNTA 3 ----- */
+const respuestas3 = {
+    dead3: "tupac"
+};
+
+/* ----- PREGUNTA 4 ----- */
+const respuestas4 = {
+    slim4: "eminem"
+};
+
+/* ----- PREGUNTA 5 ----- */
+const respuestas5 = {
+    goodDay5: "cube"
+};
