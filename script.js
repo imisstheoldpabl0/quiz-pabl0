be2065e1b772be3c92e695483b471562f2801bc5
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-03-01 10:29:04 +0100

    añadido contador/intento de pintar preguntas erroneas y correctas/ añadidas alertas

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
 /* ----- RESPUESTAS ----- */
 const respuestas = {
-    dark_twisted1: "kanye",
-    paris2: "kanye",
-    paris2: "jayz",
-    dead3: "tupac",
-    slim4: "eminem",
-    goodDay5: "cube"
+    pregunta1: "kanye1",
+    pregunta2: "kanye2",
+    pregunta3: "tupac3",
+    pregunta4: "eminem4",
+    pregunta5: "cube5"
 };
 
 
 
-document.querySelector('form[name="hip_hop"]').addEventListener('submit', (event) => {
+document.getElementById('hip_hop_quiz').addEventListener('submit', (event) => {
     event.preventDefault()
 
-    console.log('dark_twisted1', event.target.elements.dark_twisted.value)
-    console.log('dark_twisted1', event.target.elements['dark_twisted1'].value)
+    // SE DECLARA LA PUNTUACIÓN A 0 PUNTOS
+    let puntuacion = 0;
 
-    console.log('paris2', event.target.elements.dark_twisted.value)
-    console.log('paris2', event.target.elements['paris2'].value)
-})
+    // SE DECLARA EL NÚMERO DE RESPUESTAS TOTALES COMO LA LONGITUD DEL OBJETO DE RESPUESTAS
+    const preguntasTotales = Object.keys(respuestas).length;
+
+    // ITERA PARA CONTAR LA PUNTUACIÓN DEL USUARIO
+    for (let i = 0; i < preguntasTotales; i++) {
+        let respuestaUsuario = document.querySelector(`input[name="pregunta${i+1}"]:checked`)?.value;
+        if (respuestaUsuario === respuestas[`pregunta${i+1}`]) {
+            puntuacion++;
+        }
+    }
+
+    //OG
+    const pregunta1 = event.target.pregunta1.value;
+    const pregunta2 = event.target.pregunta2.value;
+    const pregunta3 = event.target.pregunta3.value;
+    const pregunta4 = event.target.pregunta4.value;
+    const pregunta5 = event.target.pregunta5.value;
+
+    // PINTAR ERRONEOS
+
+    for (let i = 0; i < preguntasTotales; i++) {
+        let respuestaUsuario = document.querySelector(`input[name="pregunta${i+1}"]:checked`)?.value;
+        if (respuestaUsuario != respuestas[`pregunta${i+1}`]) {
+            alert(`La pregunta${i+1} está mal`)
+        }
+    }
+
+    // VALIDACIONES
+
+    if (pregunta1 == "" || pregunta2 == "" || pregunta3 == "" || pregunta4 == "" || pregunta5 == "") {
+        alert("Asegúrate de haber seleccionado todas las opciones");
+    }
+
+    else {
+        //Enviar formulario
+        //document.querySelector("form").submit();
+
+        // ALERTA DE LA PUNTUACIÓN FINAL AL USUARIO
+        alert(`Tu puntuación es de ${puntuacion} sobre ${preguntasTotales}!`);
+
+        alert("GRACIAS POR PARTICIPAR!");
+        event.target.submit();
+    }
+
+
+});
